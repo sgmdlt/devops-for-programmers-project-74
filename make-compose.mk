3d37db1508059226a85d16e2ec14c75cbea75648
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-02-03 15:43:43 +0400

    add push.yml

--- make-compose.mk
+++ make-compose.mk
@@ -1,5 +1,8 @@
 compose:
 	docker compose up -d
 
-compose-test:
+compose-build:
+	docker compose build
+
+ci:
 	docker compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app
