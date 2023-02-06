b88a275d2689f443cb9502d863b9596f5d467d92
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-02-07 00:46:54 +0400

    fix push.yml

--- make-compose.mk
+++ make-compose.mk
@@ -6,9 +6,10 @@ compose-build:
 
 compose-ci-build:
 	docker build -f Dockerfile.production -t sgmdlt/devops-project-1-app .
+	docker compose -f docker-compose.yml build
 
 compose-ci: compose-ci-build
 	docker compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app
 
-copy_env:
+copy-env:
 	cp -n .env.example .env
