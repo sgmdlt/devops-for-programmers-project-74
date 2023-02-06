abad23ad80728cb10c8f0bd3c5e892177197204e
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2023-02-06 18:37:45 +0400

    fix ci

--- make-compose.mk
+++ make-compose.mk
@@ -4,8 +4,12 @@ compose:
 compose-build:
 	docker compose build
 
-ci: copy_env
+compose-ci-build:
+	docker build -f Dockerfile.production -t sgmdlt/devops-project-1-app .
+	docker compose -f docker-compose.yml build
+
+compose-ci: compose-ci-build copy_env
 	docker compose -f docker-compose.yml up --abort-on-container-exit --exit-code-from app
 
 copy_env:
-	cp -n .env.example .env || true
+	cp -n .env.example .env
